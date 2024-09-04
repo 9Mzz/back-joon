59361dacf26645848c46a9d8718299d749bdf3b2
05016 <nico1a@naver.com>
2024-09-05 00:30:40 +0900

    9/05 backjoon bronze

--- bronze/src/main/java/com/hello/bronze/B10818.java
+++ bronze/src/main/java/com/hello/bronze/B10818.java
@@ -1,8 +1,8 @@
-package com.hello;
+package com.hello.bronze;
 
 import java.util.Scanner;
 
-public class Main {
+public class B10818 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
