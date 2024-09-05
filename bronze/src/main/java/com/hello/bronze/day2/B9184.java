e2d0b139a84956b100614b0432315e656129f4ea
05016 <nico1a@naver.com>
2024-09-06 00:03:26 +0900

    9/05 backjoon bronze - 2일차

---
+++ bronze/src/main/java/com/hello/bronze/day2/B9184.java
@@ -0,0 +1,59 @@
+package com.hello.bronze.day2;
+
+import java.util.Scanner;
+
+public class B9184 {
+
+    // 2일차 - 4
+    // 메모리제이션
+    // 참고
+    // https://wondytyahng.tistory.com/entry/memoization-%EB%A9%94%EB%AA%A8%EC%9D%B4%EC%A0%9C%EC%9D%B4%EC%85%98
+    // https://gaybee.tistory.com/45
+
+    static int[][][] arrays = new int[21][21][21];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int     a  = 0;
+        int     b  = 0;
+        int     c  = 0;
+        while(true) {
+            a = sc.nextInt();
+            b = sc.nextInt();
+            c = sc.nextInt();
+            if(a == -1 && b == -1 && c == -1) {
+                break;
+
+            }
+            System.out.printf("w(%d, %d, %d) = %d\n", a, b, c, w(a, b, c));
+
+        }
+        sc.close();
+
+    }
+
+    static int w(int a, int b, int c) {
+        if(a <= 0 || b <= 0 || c <= 0) {
+            return 1;
+        }
+        if(a > 20 || b > 20 || c > 20) {
+            return w(20, 20, 20);
+        }
+
+        // 만약 계산을 한 적이 있다면, 아래 Logic을 거치지 않고 바로 return
+        if(arrays[a][b][c] != 0) {
+            return arrays[a][b][c];
+        }
+
+
+        // Logic
+        if(a < b && b < c) {
+            arrays[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+            return arrays[a][b][c];
+        }
+        // 3차원 함수에 값 저장
+        arrays[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+        return arrays[a][b][c];
+    }
+
+}
