d7151792bb0cd308c942824b4be5e3c816201012
05016 <nico1a@naver.com>
2024-09-05 10:23:46 +0900

    9/05 backjoon bronze

--- bronze/src/main/java/com/hello/bronze/B10870.java
+++ bronze/src/main/java/com/hello/bronze/B10870.java
@@ -12,7 +12,7 @@ public class B10870 {
 
     }
 
-    public static int fibonacci(int n) {
+     static int fibonacci(int n) {
         if (n == 0) return 0;
         if (n == 1) return 1;
         return fibonacci (n - 2) + fibonacci (n - 1);
