8e2361e3ec598e80706db05f3cbca82a03d1673c
05016 <nico1a@naver.com>
2024-09-07 16:57:53 +0900

    backjoon bronze - 3일차

--- bronze/src/main/java/com/hello/bronze/day3/B27160.java
+++
@@ -1,8 +0,0 @@
-package com.hello.bronze.day3;
-
-public class B27160 {
-    public static void main(String[] args) {
-        // 3일차 - 4
-        // https://www.acmicpc.net/problem/27160
-    }
-}
---
+++ bronze/src/main/java/com/hello/bronze/week1/day3/B27160.java
@@ -0,0 +1,24 @@
+package com.hello.bronze.week1.day3;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class B27160 {
+    public static void main(String[] args) throws IOException {
+        // 3일차 - 4
+        // https://www.acmicpc.net/problem/27160
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        A = Integer.parseInt(new StringBuilder().append(A)
+                .reverse()
+                .toString());
+        B = Integer.parseInt(new StringBuilder().append(B)
+                .reverse()
+                .toString());
+
+        System.out.print(A > B ? A : B);
+        sc.close();
+    }
+}
