cf35a0842021d49d055bff9079026319f5f037d7
BackPicker <93580898+BackPicker@users.noreply.github.com>
2024-09-27 20:43:08 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14508 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,35 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.util.HashMap;
-import java.util.Map;
+import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        BufferedReader br        = new BufferedReader(new InputStreamReader(System.in));
+        int            L         = Integer.parseInt(br.readLine());
+        String         numString = br.readLine();
 
-        // 사전 작업 -> 소문자로 이루어진 문자열을 숫자로 변환
-        // 이후 map에 put
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i <= 25; i++) {
-            char ch = (char) (i + 97);
-            map.put(ch, i + 1);
-        }
-        // 값을 출력할 result 설정
-        int result = 0;
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int            L  = Integer.parseInt(br.readLine());
-        if (L < 0 || L > 51) {
-            System.exit(0);
+        // BigInt 설정하기
+        BigInteger bigint = BigInteger.ZERO;
+        BigInteger r      = BigInteger.valueOf(31); // r의 값은 26보다 큰 소수인 31로 하고
+        BigInteger M      = BigInteger.valueOf(1234567891); // M의 값은 1234567891(놀랍게도 소수이다!!)로 하자.
+
+        for (int i = 0; i < L; i++) {
+            int        num     = numString.charAt(i) - 96;
+            BigInteger bigNum  = BigInteger.valueOf(num);
+            BigInteger squareR = r.pow(i);
+            bigint = bigint.add(bigNum.multiply(squareR)).mod(M);
+
         }
-        Array[]  arrays = new Array[L];
-        String[] split  = br.readLine().trim().split("");
-        for (int i = 0; i < split.length; i++) {
-            Integer value = map.get(split[i].charAt(0));
-            int     total = (int) (value * (Math.pow(31, i)));
-            result = result + total;
-        }
-        System.out.println(result);
+        System.out.println(bigint);
+
+
     }
 }
