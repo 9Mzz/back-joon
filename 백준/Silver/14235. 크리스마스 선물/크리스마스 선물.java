092f376132952ed6069554825c680a11936c5e60
BackPicker <93580898+BackPicker@users.noreply.github.com>
2024-09-29 23:34:36 +0900

    [Silver III] Title: 크리스마스 선물, Time: 328 ms, Memory: 35780 KB -BaekjoonHub

--- 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.java
+++ 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.java
@@ -1,7 +1,5 @@
 
-
 import java.io.*;
-import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
@@ -9,10 +7,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader         br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter         bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
 
-        int query = Integer.parseInt(br.readLine());
-        for (int i = 0; i < query; i++) {
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int             a  = Integer.parseInt(st.nextToken());
             if (a == 0) {
@@ -21,12 +20,14 @@ public class Main {
                 } else {
                     bw.write(pq.poll() + "\n");
                 }
+                // 아닌 경우
             } else {
                 for (int j = 0; j < a; j++) {
                     pq.add(Integer.parseInt(st.nextToken()));
                 }
             }
         }
+
 
         bw.flush();
         bw.close();
