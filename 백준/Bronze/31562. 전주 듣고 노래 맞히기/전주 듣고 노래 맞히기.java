6f38b6bc4adb7c9f2051582f8597ae4353078647
BackPicker <93580898+BackPicker@users.noreply.github.com>
2024-10-03 00:48:33 +0900

    [Bronze I] Title: 전주 듣고 노래 맞히기, Time: 168 ms, Memory: 17412 KB -BaekjoonHub

--- 백준/Bronze/31562. 전주 듣고 노래 맞히기/전주 듣고 노래 맞히기.java
+++ 백준/Bronze/31562. 전주 듣고 노래 맞히기/전주 듣고 노래 맞히기.java
@@ -1,52 +1,50 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        StringTokenizer st;
-        BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
-        int                       N   = Integer.parseInt(st.nextToken());
-        int                       M   = Integer.parseInt(st.nextToken());
-        Map<String, List<String>> map = new HashMap<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer           st      = new StringTokenizer(br.readLine());
+        int                       N       = Integer.parseInt(st.nextToken());
+        int                       M       = Integer.parseInt(st.nextToken());
+        Map<String, List<String>> nameMap = new HashMap<>();
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int    T = Integer.parseInt(st.nextToken());    // 노래 제목의 길이 T
-            String S = st.nextToken();  // 문자열 노래 제목 S
-            if (S.length() != T) {
-                continue;
-            }
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < 3; j++) {
-                sb.append(st.nextToken());
-            }
-            String questionToneName = sb.toString();
-
-            map.putIfAbsent(questionToneName, new ArrayList<>());
-            map.get(questionToneName).add(S);   // Map Value(List)에 제목을 집어넣음
+            st.nextToken(); // Skip first token
+            String name      = st.nextToken();
+            String resultKey = st.nextToken() + st.nextToken() + st.nextToken();
+            nameMap.putIfAbsent(resultKey, new ArrayList<>());
+            nameMap.get(resultKey)
+                    .add(name);
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < 3; j++) {
+            while (st.hasMoreTokens()) {
                 sb.append(st.nextToken());
             }
-            String answerToneName = sb.toString();
+            String       key          = sb.toString();
+            List<String> matchedNames = nameMap.getOrDefault(key, Collections.emptyList());
 
-            List<String> quizResult = map.getOrDefault(answerToneName, new ArrayList<>());
-            if (quizResult.size() > 1) {
-                System.out.println("?");
-            } else if (quizResult.size() == 1) {
-                System.out.println(quizResult.get(0));
+            if (matchedNames.size() == 1) {
+                bw.write(matchedNames.get(0));
+                bw.newLine();
+            } else if (matchedNames.size() > 1) {
+                bw.write("?");
+                bw.newLine();
             } else {
-                System.out.println("!");
+                bw.write("!");
+                bw.newLine();
             }
         }
+
+        bw.flush();
+        bw.close();
         br.close();
     }
 }
