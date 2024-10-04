fdb86cf651e5ce6d15861546187e13a78e251f41
BackPicker <93580898+BackPicker@users.noreply.github.com>
2024-10-04 09:51:22 +0900

    [Silver I] Title: 신입 사원, Time: 2788 ms, Memory: 313628 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -1,5 +1,4 @@
 
-
 import java.io.*;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -7,10 +6,10 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter  bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //        BufferedWriter  bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
-        int             T  = Integer.parseInt(br.readLine());
+        int             T = Integer.parseInt(br.readLine());
         for (int i = 0; i < T; i++) {
 
             int     N            = Integer.parseInt(br.readLine());
